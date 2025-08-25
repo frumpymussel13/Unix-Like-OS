80c1e501efc6490e78fc13e31799bd095a1da2a4
Walter <walter.h.warriner@outlook.com>
2025-08-25 12:26:23 -0500

    Added Helo message

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 nasm src/main.asm -f bin -o build/main.bin
+sleep 1
 cp build/main.bin build/main_floppy.img
+sleep 1
 truncate -s 1440k build/main_floppy.img
+sleep 1
 qemu-system-i386 -fda build/main_floppy.img
--- src/main.asm
+++ src/main.asm
@@ -2,11 +2,50 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+start:
+    jmp main
+
+puts:
+    push si
+    push ax
+
+.loop:
+    lodsb
+    or al, al
+    jz .done
+    jmp .loop
+    mov ah, 0x0e
+    int 0x10
+
+
+
+
+.done:
+    pop ax
+    pop si
+    ret
+
+
 main:
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    mov ss, ax
+    mov sp, 0x7C80
+
+    mov si, msg_Hello
+    call puts
+
+
     hlt
 
 .halt
     jmp .halt
+
+msg_Hello: 'Helo, Welcome to my Unix Like OS!', ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
